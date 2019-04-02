e78e91f5669226c80cc7e0af533ac4afcaa9469e
schifferl <schifferl@users.noreply.github.com>
2019-04-02 23:09:37 +0000

    subsetting by genotoxicity

---
+++ data-raw/miniGenTox.R
@@ -0,0 +1,57 @@
+library(magrittr)
+
+load("~/SSML/inst/extdata/DrugMatrix.rda")
+
+extract_colData <- function(x) {
+    SummarizedExperiment::colData(x) %>%
+        S4Vectors::as.data.frame() %>%
+        tibble::rownames_to_column()
+}
+
+extract_assay <- function(x) {
+    SummarizedExperiment::assay(x) %>%
+        base::t() %>%
+        S4Vectors::as.data.frame() %>%
+        tibble::rownames_to_column()
+}
+
+prepare_data <- function(x) {
+    base::list(extract_colData, extract_assay) %>%
+        purrr::invoke_map(x = x) %>%
+        purrr::reduce(base::merge.data.frame)
+}
+
+not_all_na <- function(x) {
+    base::is.na(x) %>%
+        base::all() %>%
+        base::isFALSE()
+}
+
+not_zero_variance <- function(x) {
+    base::unique(x) %>%
+        base::length() %>%
+        magrittr::is_greater_than(1)
+}
+
+DrugMatrix <-
+    prepare_data(DrugMatrix) %>%
+    dplyr::filter(ORGAN.OR.CELL.TYPE == "LIVER") %>%
+    tibble::column_to_rownames() %>%
+    dplyr::select(18, 27:11270) %>%
+    dplyr::select_if(not_all_na) %>%
+    dplyr::select_if(not_zero_variance)
+
+top_features <-
+    dplyr::select(DrugMatrix, -1) %>%
+    base::apply(2, stats::mad) %>%
+    base::sort(decreasing = TRUE, index.return = TRUE) %>%
+    magrittr::extract2(2) %>%
+    magrittr::add(1) %>%
+    magrittr::extract(1:5000)
+
+miniGenTox <-
+    dplyr::select(DrugMatrix, GenTox, top_features) %>%
+    dplyr::rename(Genotoxic = GenTox) %>%
+    dplyr::mutate(Genotoxic = forcats::as_factor(Genotoxic))
+
+usethis::use_data(miniGenTox)
